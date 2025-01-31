3cb8e025ce897e9752bba4330bdae3dcbbc27082
0tohero99 <shubhamsinghhindustan@gmail.com>
2025-01-30 23:32:16 -0800

    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+const workEx =()=>{
+    document.getElementById("link_work_Ex").style.display="block";
+    document.getElementById("codeworks").style.display="none";
+}
+const codeworks =()=>{
+    document.getElementById("link_work_Ex").style.display="none";
+    document.getElementById("codeworks").style.display="block";
+}
